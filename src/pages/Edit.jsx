e52de47047d0ee1e9e9174fb9aafa2d350b664f9
renad-lab <renad@pursuit.org>
2024-05-31 11:45:18 -0400

    CRUD practice

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,88 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Edit = () => {
-    const API = import.meta.env.VITE_BASE_URL
-    const [bookmark, setBookmark] = useState({
-        name: "",
-        url: "",
-        category: "",
-        is_favorite: false
+  const API = import.meta.env.VITE_BASE_URL;
+  const [bookmark, setBookmark] = useState({
+    name: "",
+    url: "",
+    category: "",
+    is_favorite: false,
+  });
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`${API}/${id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log("Edit page: ", res)
+        setBookmark((prevState) => res);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  const handleChange = (e) => {
+    // console.log(e)
+    setBookmark((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCheckBox = (e) => {
+    setBookmark((prevState) => {
+      const favorited = !bookmark.is_favorite;
+      return { ...prevState, is_favorite: favorited };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`${API}/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(bookmark),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    const navigate = useNavigate()
-    const { id } = useParams()
+      .then((res) => res.json())
+      .then(() => navigate(`/bookmarks/${id}`))
+      .catch((err) => console.log(err));
+  };
 
-    useEffect(() => {
-        fetch(`${API}/${id}`)
-            .then(res => res.json())
-            .then(res => {
-                // console.log("Edit page: ", res)
-                setBookmark((prevState) => res)
-            })
-            .catch(err => console.log(err))
-    }, [id])
-
-    const handleChange = (e) => {
-        // console.log(e)
-        setBookmark((prevState) => {
-            return { ...prevState, [e.target.name]: e.target.value }
-        })
-    }
-
-    const handleCheckBox = (e) => {
-        setBookmark((prevState) => {
-            const favorited = !bookmark.is_favorite
-            return { ...prevState, is_favorite: favorited }
-        })
-    }
-
-    const handleSubmit = () => {
-
-    }
-
-    if(!bookmark) return <div>Loading...</div>
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <legend>Edit this Bookmark</legend>
-                    <input 
-                        type="text"
-                        placeholder="Bookmark Name"
-                        name="name"
-                        value={bookmark.name}
-                        onChange={handleChange}
-                    />
-                    <br/>
-                    <input 
-                        type="text" 
-                        placeholder='URL'
-                        name="url"
-                        value={bookmark.url}
-                        onChange={handleChange}
-                    />
-                    <br/>
-                    <input 
-                        type="text" 
-                        placeholder='Category'
-                        name="category"
-                        value={bookmark.category}
-                        onChange={handleChange}
-                    />
-                    <br/>
-                    <input 
-                        type="checkbox"
-                        id="fav"
-                        checked={bookmark.is_favorite}
-                        onChange={handleCheckBox}
-                    />
-                    <label htmlFor="fav">Favorite</label>
-                    <br/>
-                    <input type="submit" value="Submit"/>
-                </fieldset>
-            </form>
-        </div>
-    );
+  if (!bookmark) return <div>Loading...</div>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Edit this Bookmark</legend>
+          <input
+            type="text"
+            placeholder="Bookmark Name"
+            name="name"
+            value={bookmark.name}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="URL"
+            name="url"
+            value={bookmark.url}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Category"
+            name="category"
+            value={bookmark.category}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="checkbox"
+            id="fav"
+            checked={bookmark.is_favorite}
+            onChange={handleCheckBox}
+          />
+          <label htmlFor="fav">Favorite</label>
+          <br />
+          <input type="submit" value="Submit" />
+        </fieldset>
+      </form>
+      <Link to={`/bookmarks/${id}`}>
+        <button>Back</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Edit;
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,37 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 
 const Show = () => {
-    const { id } = useParams()
-    const API = import.meta.env.VITE_BASE_URL
-    const [bookmark, setBookmark] = useState({
-        name: "",
-        url: "",
-        category: "",
-        is_favorite: false
+  const { id } = useParams();
+  const API = import.meta.env.VITE_BASE_URL;
+  const [bookmark, setBookmark] = useState({
+    name: "",
+    url: "",
+    category: "",
+    is_favorite: false,
+  });
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    fetch(`${API}/${id}`, {
+      method: "DELETE",
     })
+      .then((res) => res.json())
+      .then(() => navigate("/"))
+      .catch((err) => console.log(err));
+  };
 
-    useEffect(() => {
-        //                      API                               ID
-        // https://bookmarks-api-720.onrender.com/bookmarks + / + 5
-        fetch(`${API}/${id}`)
-            .then(res => res.json())
-            .then(res => {
-                setBookmark((prevState) => res)
-            })
-        }, [id])
+  useEffect(() => {
+    //                      API                               ID
+    // https://bookmarks-api-720.onrender.com/bookmarks + / + 5
+    fetch(`${API}/${id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setBookmark((prevState) => res);
+      });
+  }, [id]);
 
-    return (
-        <div>
-            <h3>{bookmark.name}</h3>
-            <a href={bookmark.url} target="_blank">{bookmark.url}</a>
-            <p>Category: {bookmark.category}</p>
-            <p>Favorite: {bookmark.is_favorite ? "true" : "false"}</p>
-            <Link to={`/bookmarks/${id}/edit`}>
-                <button>Edit</button>
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <h3>{bookmark.name}</h3>
+      <a href={bookmark.url} target="_blank">
+        {bookmark.url}
+      </a>
+      <p>Category: {bookmark.category}</p>
+      <p>Favorite: {bookmark.is_favorite ? "true" : "false"}</p>
+      <Link to={`/bookmarks/${id}/edit`}>
+        <button>Edit</button>
+      </Link>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 };
 
 export default Show;
